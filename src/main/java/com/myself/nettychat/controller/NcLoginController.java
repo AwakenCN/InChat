a2b6a20e596c42b18f080fd5451e22f9a00e4d12
胡宏韬 <3183764662@qq.com>
2018-11-02 17:02:58 +0800

    对【账号注册】API加日志输出与相关注释

--- src/main/java/com/myself/nettychat/controller/NcLoginController.java
+++ src/main/java/com/myself/nettychat/controller/NcLoginController.java
@@ -11,6 +11,7 @@ import com.myself.nettychat.service.UserService;
 import com.myself.nettychat.store.TokenStore;
 import com.myself.nettychat.common.utils.CookieUtil;
 import com.myself.nettychat.vo.ResultVo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -25,11 +26,13 @@ import java.util.Map;
 import java.util.UUID;
 
 /**
+ * 用户账号模块
  * @Author:UncleCatMySelf
  * @Email：zhupeijie_java@126.com
  * @QQ:1341933031
  * @Date:Created in 16:01 2018\8\18 0018
  */
+@Slf4j
 @RestController
 @RequestMapping("/user")
 public class NcLoginController {
@@ -38,20 +41,23 @@ public class NcLoginController {
     private UserService userService;
 
     /**
-     *
-     * @param username
-     * @param password
-     * @return
+     * 【账号注册】-2018-11-2
+     * @param username 用户名
+     * @param password 密码
+     * @return {@link ResultVo}
      */
     @PostMapping(value = "/to_register")
     public ResultVo toRegister(@RequestParam("username") String username,@RequestParam("password") String password){
+        //参数非空判断，如前端实现，可省略
         if (username.isEmpty() | password.isEmpty()){
+            log.info("user-【参数为空】");
             return ResultVOUtil.error(ResultConstant.PARAMS_CODE,ResultConstant.PARAMS_MSG);
         }
         //TODO 用户过多，需优化
         List<User> userList = userService.findAll();
         for (User item:userList){
             if (item.getUserName().equals(username)){
+                log.info("user-【用户名已存在】-"+username);
                 return ResultVOUtil.error(ResultConstant.USER_NAME_REPART_CODE,ResultConstant.USER_NAME_REPART_MSG);
             }
         }
@@ -59,50 +65,36 @@ public class NcLoginController {
         user.setUserName(username);
         user.setPassWord(password);
         userService.save(user);
+        log.info("user-【账号注册成功】-"+username);
         return ResultVOUtil.success("【账号注册成功】");
     }
 
-    /**
-     * 登录判断
-     * @return
-     */
-    @PostMapping("/toLogin")
-    public ModelAndView toLogin(@RequestParam(value = "page",defaultValue = "1") Integer page,
-                                @RequestParam(value = "size",defaultValue = "10") Integer size,
-                                @Valid LoginForm form, BindingResult bindingResult , HttpServletResponse response,
-                                Map<String, Object> map){
-        if (bindingResult.hasErrors()){
-            map.put("msg",bindingResult.getFieldError().getDefaultMessage());
-            return new ModelAndView(H5Constant.LOGIN_SUI,map);
+
+    @PostMapping(value = "/to_login")
+    public ResultVo toLogin(@RequestParam("username") String username,@RequestParam("password") String password){
+        //参数非空判断，如前端实现，可省略
+        if (username.isEmpty() | password.isEmpty()){
+            return ResultVOUtil.error(ResultConstant.PARAMS_CODE,ResultConstant.PARAMS_MSG);
         }
-        try {
-            User user = userService.findByUserName(form.getFUserName());
-            if (user.getPassWord().equals(form.getFPassWord())){
-                //登录成功
-                String token = UUID.randomUUID().toString();
-                //将token信息添加到系统缓存中
-                TokenStore.add(token,user.getId());
-                //将Token信息添加到Cookie中
-                CookieUtil.set(response, CookieConstant.TOKEN,token,CookieConstant.EXPIRE);
-//                Sort sort = new Sort(Sort.Direction.DESC,"id");
-//                Pageable pageable = new PageRequest(page-1,size,sort);
-//                Page<UserMsg> userMsgPage = userMsgRepository.findAll(pageable);
-//                //日期颠倒
-//                List<UserMsg> userMsgList = new ArrayList<>();
-//                for (int i = 0,j = userMsgPage.getContent().size()-1; i < userMsgPage.getContent().size();i++,j--){
-//                    userMsgList.add(userMsgPage.getContent().get(j));
-//                }
-//                map.put("userMsgList",userMsgList);
-//                map.put("userName",user.getUserName());
-                return new ModelAndView(H5Constant.HOME);
-            }else{
-                map.put("msg","密码错误");
-                return new ModelAndView(H5Constant.LOGIN_SUI,map);
-            }
-        }catch (Exception e){
-            map.put("msg","用户不存在");
-            return new ModelAndView(H5Constant.LOGIN_SUI,map);
-        }
+//        try {
+//            User user = userService.findByUserName(form.getFUserName());
+//            if (user.getPassWord().equals(form.getFPassWord())){
+//                //登录成功
+//                String token = UUID.randomUUID().toString();
+//                //将token信息添加到系统缓存中
+//                TokenStore.add(token,user.getId());
+//                //将Token信息添加到Cookie中
+//                CookieUtil.set(response, CookieConstant.TOKEN,token,CookieConstant.EXPIRE);
+//                return new ModelAndView(H5Constant.HOME);
+//            }else{
+//                map.put("msg","密码错误");
+//                return new ModelAndView(H5Constant.LOGIN_SUI,map);
+//            }
+//        }catch (Exception e){
+//
+//            return new ModelAndView(H5Constant.LOGIN_SUI,map);
+//        }
+        return ResultVOUtil.success();
     }
 
 }
