d5a78c3e580fd7036eb0bf3a945b509f31a53496
胡宏韬 <3183764662@qq.com>
2018-11-06 14:46:41 +0800

    添加对应api

--- src/main/java/com/myself/nettychat/controller/NCBackController.java
+++ src/main/java/com/myself/nettychat/controller/NCBackController.java
@@ -1,11 +1,10 @@
 package com.myself.nettychat.controller;
 
+import com.myself.nettychat.common.utils.Const;
 import com.myself.nettychat.common.utils.ResultVOUtil;
 import com.myself.nettychat.common.utils.SendUtil;
-import com.myself.nettychat.constont.LikeRedisTemplate;
 import com.myself.nettychat.vo.ResultVo;
 import io.netty.channel.Channel;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -18,6 +17,40 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/back")
 public class NCBackController {
 
+    /**
+     * 获取当前连接数
+     * @return
+     */
+    @GetMapping(value = "/get_channel_size")
+    public ResultVo getChannelSize(){
+        return ResultVOUtil.success(Const.getSzie());
+    }
 
+    /**
+     * 获取连接Id数组
+     * @return
+     */
+    @GetMapping(value = "/get_channel_id_list")
+    public ResultVo getChannelIDList(){
+        return ResultVOUtil.success(Const.getIdList());
+    }
+
+    /**
+     * 向存在链接发送指定的端口
+     * @param channelId 连接Id
+     * @param lock 打开第几把锁
+     * @return
+     */
+    @PostMapping(value = "/send_to_channel")
+    public ResultVo SendToChannel(String channelId,Integer lock){
+        SendUtil sendUtil = new SendUtil();
+        Channel channel = Const.get(channelId);
+        if (channel != null){
+            if (sendUtil.send(lock,channel,channelId,Const.CONTROL_TYPE)){
+                return ResultVOUtil.success("【发送成功】");
+            }
+        }
+        return ResultVOUtil.error(888,"【发送失败】");
+    }
 
 }
