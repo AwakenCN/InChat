65421747091f861b0263085a1315e68aec10e7cf
朱培杰 <3183764662@qq.com>
2018-12-21 10:52:56 +0800

    服务发送接口测试

--- src/main/java/com/github/unclecatmyself/Application.java
+++ src/main/java/com/github/unclecatmyself/Application.java
@@ -2,9 +2,15 @@ package com.github.unclecatmyself;
 
 import com.github.unclecatmyself.auto.ConfigFactory;
 import com.github.unclecatmyself.auto.InitServer;
+import com.github.unclecatmyself.bootstrap.channel.WebSocketChannelService;
 import com.github.unclecatmyself.common.bean.InitNetty;
 import com.github.unclecatmyself.user.DataBaseServiceImpl;
 import com.github.unclecatmyself.user.InChatVerifyServiceImpl;
+import io.netty.channel.Channel;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 /**
  * Created by MySelf on 2018/12/18.
@@ -12,16 +18,32 @@ import com.github.unclecatmyself.user.InChatVerifyServiceImpl;
 public class Application {
 
     public static void main(String[] args) {
-        //注册InChat相关配置
+        //配置InChat配置工厂
         ConfigFactory.inChatToDataBaseService = new DataBaseServiceImpl();
         ConfigFactory.inChatVerifyService = new InChatVerifyServiceImpl();
-
+        //默认启动InChat
         InitServer initServer = new InitServer(new InitNetty());
         initServer.open();
-        //预制InChat的用户接口
 
-        //启动InChat流程
-        
+        //获取用户值
+        WebSocketChannelService webSocketChannelService = new WebSocketChannelService();
+
+        //启动新线程
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //设定默认服务器发送值
+                Map<String,String> map = new HashMap<>();
+                map.put("server","服务器");
+                //获取控制台用户想发送的用户Token
+                Scanner scanner = new Scanner(System.in);
+                String token = scanner.nextLine();
+                //获取用户连接
+                Channel channel = (Channel) webSocketChannelService.getChannel(token);
+                //调用接口发送
+                webSocketChannelService.sendFromServer(channel,map);
+            }
+        }).start();
 
     }
 
