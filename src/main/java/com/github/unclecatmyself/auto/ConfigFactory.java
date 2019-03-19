0974ff82052a4bfbbc750512c5d6787a75e9c52f
朱培杰 <3183764662@qq.com>
2019-03-19 12:45:06 +0800

    添加部分注解

--- src/main/java/com/github/unclecatmyself/auto/ConfigFactory.java
+++ src/main/java/com/github/unclecatmyself/auto/ConfigFactory.java
@@ -11,6 +11,7 @@ import com.github.unclecatmyself.common.bean.InitNetty;
  */
 public class ConfigFactory {
 
+    /** Redis的ip地址，这你不会不知道吧？ */
     public static String RedisIP;
 
     /** 用户校验伪接口 */
@@ -19,6 +20,7 @@ public class ConfigFactory {
     /** 用户获取数据伪接口 */
     public static InChatToDataBaseService inChatToDataBaseService;
 
+    /** 系统信息枚举服务接口 */
     public static FromServerService fromServerService;
 
     /** InChat项目配置 */
--- src/main/java/com/github/unclecatmyself/auto/InitServer.java
+++ src/main/java/com/github/unclecatmyself/auto/InitServer.java
@@ -13,14 +13,20 @@ public abstract class InitServer {
 
 //    private InitNetty serverBean;
 
+    /** 静态化处理，保证唯一，确保用户启动的是他自己指定的，不是框架的
+     *  一个数据配置集合
+     */
     private static InitNetty serverBean = ConfigFactory.initNetty;
 
 //    public InitServer(InitNetty serverBean) {
 //        this.serverBean = serverBean;
 //    }
-
+    /** netty服务器启动切面 */
     static BootstrapServer bootstrapServer;
 
+    /**
+     * 主要还是这个{@link NettyBootstrapServer},实例化想要的netty配置服务
+     */
     public static void open(){
         if(serverBean!=null){
             bootstrapServer = new NettyBootstrapServer();
@@ -29,6 +35,9 @@ public abstract class InitServer {
         }
     }
 
+    /**
+     * 关闭服务
+     */
     public void close(){
         if(bootstrapServer!=null){
             bootstrapServer.shutdown();
