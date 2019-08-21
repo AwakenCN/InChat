4230773bbae627b16540e5614802f6dc0ac17ad6
朱培杰 <1341933031@qq.com>
2019-08-21 15:10:38 +0800

    测试数据存储

---
+++ src/main/java/com/github/unclecatmyself/task/ListenAsynData.java
@@ -0,0 +1,13 @@
+package com.github.unclecatmyself.task;
+
+import java.util.Map;
+
+/**
+ * 监听异步数据
+ * Created by MySelf on 2019/8/20.
+ */
+public abstract class ListenAsynData {
+
+    public abstract void asynData(Map<String,Object> maps);
+
+}
