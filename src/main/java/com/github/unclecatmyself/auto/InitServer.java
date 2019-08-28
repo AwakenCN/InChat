c074a31f1f1d5201abadfe961c1f6d7bcd978934
noseparte <noseparte@aliyun.com>
2019-08-28 17:41:21 +0800

    加锁，防止重复启动 synchronized (waitLock)

--- src/main/java/com/github/unclecatmyself/auto/InitServer.java
+++ src/main/java/com/github/unclecatmyself/auto/InitServer.java
@@ -4,6 +4,8 @@ package com.github.unclecatmyself.auto;
 import com.github.unclecatmyself.bootstrap.BootstrapServer;
 import com.github.unclecatmyself.bootstrap.NettyBootstrapServer;
 import com.github.unclecatmyself.common.bean.InitNetty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * InChat项目启动服务
@@ -11,26 +13,25 @@ import com.github.unclecatmyself.common.bean.InitNetty;
  **/
 public abstract class InitServer {
 
-//    private InitNetty serverBean;
-
-    /** 静态化处理，保证唯一，确保用户启动的是他自己指定的，不是框架的
-     *  一个数据配置集合
+    /**
+     * 静态化处理，保证唯一，确保用户启动的是他自己指定的，不是框架的
+     * 一个数据配置集合
      */
     private static InitNetty serverBean = ConfigFactory.initNetty;
 
-//    public InitServer(InitNetty serverBean) {
-//        this.serverBean = serverBean;
-//    }
-    /** netty服务器启动切面 */
+    /**
+     * netty服务器启动切面
+     */
     static BootstrapServer bootstrapServer;
 
     /**
      * 主要还是这个{@link NettyBootstrapServer},实例化想要的netty配置服务
      */
-    public static void open(){
-        if(serverBean!=null){
+    public static void open() {
+        if (serverBean != null) {
             bootstrapServer = new NettyBootstrapServer();
             bootstrapServer.setServerBean(serverBean);
+            int port = serverBean.getWebport();
             bootstrapServer.start();
         }
     }
@@ -38,8 +39,8 @@ public abstract class InitServer {
     /**
      * 关闭服务
      */
-    public void close(){
-        if(bootstrapServer!=null){
+    public void close() {
+        if (bootstrapServer != null) {
             bootstrapServer.shutdown();
         }
     }
